308bcc159e91e6fa53e73b98479fd0e11dd8c93a
Eduard Bagrov <ebagrov@gmail.com>
2011-06-09 15:35:33 +0500

    Adding FindFile benchmark test

---
+++ src/benchmark/FindFile/FindFile.hpp
@@ -0,0 +1,33 @@
+#ifndef FIND_FILE_H
+#define FIND_FILE_H
+
+#include "BenchmarkTest.hpp"
+
+enum FindFileOperations
+{
+	FindFile
+};
+
+class FindFileTest : public BenchmarkTest
+{
+public:
+		
+	FindFileTest(Mode m, int op, string a) : BenchmarkTest (m, op, a)
+	{
+		directoryName = "results";
+		fileName = "0";
+	}
+	
+	~FindFileTest()
+	{
+	}
+	
+	Status Main();
+private:
+	string fileName;
+	string directoryName;
+	Status FindFileCreationFunc();
+	string CreateCommand();
+};
+
+#endif
