3e7a64d40a10044768d14807b666ca260e8cb7fd
vmartirosyan <vmartirosyan@localhost>
2011-06-16 12:10:18 +0500

    Documentation generation on configuration stage

---
+++ src/benchmark/MultipleFilesDeletion/MultipleFilesDeletion.sh
@@ -0,0 +1,29 @@
+#      MultipleFilesDeletion.sh
+#      
+#      Copyright 2011 Tigran Piloyan <tigran.piloyan@gmail.com>
+#      
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation; either version 2 of the License, or
+#      (at your option) any later version.
+#     
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#      
+#      You should have received a copy of the GNU General Public License
+#      along with this program; if not, write to the Free Software
+#      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#      MA 02110-1301, USA.
+
+#!/bin/bash
+
+# Deleting the specified directory
+
+if [ -d $1 ]
+then 
+	rm -rf $1
+else
+	echo "The directory you specified does not exist."
+fi
---
+++ src/benchmark/main.cpp
@@ -0,0 +1,21 @@
+#include "Creator.hpp"
+#include "Deleter.hpp"
+#include "Compressor.hpp"
+#include "LargeFile.hpp"
+#include "FindFile.hpp"
+#include "LargeFileCopyTest.hpp"
+
+int main(int argc, char ** argv)
+{
+	TestCollection tests;
+	tests.AddTest(new CreatorTest(Normal, MultipleFilesCreation, ""));
+	tests.AddTest(new FindFileTest(Normal, FindFile, ""));
+	tests.AddTest(new CompressTest(Normal, MultipleFilesCompression, ""));
+	tests.AddTest(new DeleterTest(Normal, MultipleFilesDeletion, ""));
+	tests.AddTest(new LargeFileTest(Normal, LargeFileCreation, ""));
+	tests.AddTest(new LargeFileCopyTest(Normal, LargeFileCopying, ""));
+	
+	TestResultCollection res = tests.Run();
+		
+	cout << res.ToXML() << endl;
+}
