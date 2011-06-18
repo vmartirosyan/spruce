64b1de8665e81279569378a4655d69f4433f3938
Eduard Bagrov <ebagrov@gmail.com>
2011-06-18 16:12:58 +0500

    Added EFAULT error test for read function

---
+++ src/benchmark/MultipleFilesCompression/Compressor.cpp
@@ -0,0 +1,58 @@
+//      Compressor.cpp
+//      
+//      Copyright 2011 Eduard Bagrov <ebagrov@gmail.com>
+//      
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//      
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//      
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//      MA 02110-1301, USA.
+
+#include <stdlib.h>
+#include <memory>
+#include "Compressor.hpp"
+#include "UnixCommand.hpp"
+
+using std::auto_ptr;
+
+int CompressTest::Main(vector<string>)
+{
+	if ( _mode == Normal )
+	{	
+		switch (_operation)
+		{
+			case MultipleFilesCompression:
+				return MultipleFilesCompressionFunc();
+			default:
+				cerr << "Unsupported operation.";	
+				return Unres;		
+		}
+	}
+	cerr << "Test was successful";
+	return Success;
+}
+
+Status CompressTest::MultipleFilesCompressionFunc()
+{
+	UnixCommand command("./MultipleFilesCompression.sh");
+    auto_ptr<ProcessResult> result(command.Execute(CreateArguments()));
+    cerr << result->GetOutput() << " ";
+    
+    return (Status)result->GetStatus();
+}
+
+vector<string> CompressTest::CreateArguments()
+{
+	vector<string> command;
+	command.push_back(directory);
+	return command;
+}
---
+++ src/syscall/main.cpp
@@ -0,0 +1,24 @@
+#include "SyscallTest.hpp"
+#include "DupFileDescriptor.hpp"
+#include "ReadWriteFile.hpp"
+#include "Close.hpp"
+
+int main(int argc, char ** argv)
+{
+	TestCollection tests;
+	
+	tests.AddTest(new DupFileDescriptorTest(Normal, Dup));
+	tests.AddTest(new ReadWriteFileTest(Normal, ReadBadFileDescriptor1, ""));
+	tests.AddTest(new ReadWriteFileTest(Normal, ReadBadFileDescriptor2, ""));
+	tests.AddTest(new ReadWriteFileTest(Normal, ReadEinvalError, ""));
+	tests.AddTest(new ReadWriteFileTest(Normal, ReadIsdirError, ""));
+	tests.AddTest(new ReadWriteFileTest(Normal, ReadEfaultError, ""));
+	tests.AddTest(new ReadWriteFileTest(Normal, proba, ""));
+	tests.AddTest(new Close(Normal, BadFileDescriptor, ""));
+ 	tests.AddTest(new Close(Normal, CorrectDescriptor, ""));
+	tests.AddTest(new Close(Normal, CorrectDescriptorUnlink, ""));
+	
+	TestResultCollection res = tests.Run();
+	
+	cout << res.ToXML() << endl;	
+}
