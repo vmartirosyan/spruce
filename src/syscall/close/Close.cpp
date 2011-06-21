d675cf629db3b838ca406ba57581a38c86a0c7d3
gurgen.torozyan <gurgen.torozyan@localhost>
2011-06-21 10:58:53 +0500

    ff

--- src/syscall/close/Close.cpp
+++ src/syscall/close/Close.cpp
@@ -17,7 +17,7 @@
 //      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 //      MA 02110-1301, USA.
 
-#include "Chmod.hpp"
+#include "Close.hpp"
 #include <unistd.h>
 #include <fcntl.h>
 #include "File.hpp"
