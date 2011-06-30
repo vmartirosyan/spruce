bc5faaf7fed3b16d6e3ba8de71fd2dc5f7620fe4
Eduard Bagrov <ebagrov@gmail.com>
2011-06-30 10:20:49 +0500

    Added getdents general case

---
+++ src/benchmark/LargeFileCreation/LargeFileCreation.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#      LargeFileCreation.sh
+#      
+# 		Copyright (C) 2011, Institute for System Programming
+#                          of the Russian Academy of Sciences (ISPRAS)
+#
+#      Author: Eduard Bagrov <ebagrov@gmail.com>
+#      
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation; either version 2 of the License, or
+#      (at your option) any later version.
+#      
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#      
+#      You should have received a copy of the GNU General Public License
+#      along with this program; if not, write to the Free Software
+#      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#      MA 02110-1301, USA.
+
+
+# creates large file with fixed size filled with zeros
+# arguments
+# - name of file to create
+# - size of file to create
+# 'size' can be followed by the following multiplicative suffixes:
+# c = 1, w = 2,  b = 512, kB = 1000, K = 1024, MB = 1000*1000, M =1024*1024, 
+# xM = M GB = 1000*1000*1000, G = 1024*1024*1024, and so on for T, P, E, Z, Y.
+
+parent_folder="results"
+mkdir -p $parent_folder
+cd $parent_folder
+touch $1
+
+# creating and filling the existing file with zeros
+${CMAKE_INSTALL_PREFIX}bin/createfile "1" $2
+
---
+++ src/utils/test.cpp
@@ -0,0 +1,84 @@
+//      test.hpp
+//      
+//      Copyright (C) 2011, Institute for System Programming
+//                          of the Russian Academy of Sciences (ISPRAS)
+//      Author:
+//			Vahram Martirosyan <vmartirosyan@gmail.com>
+//      
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//      
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//      
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//      MA 02110-1301, USA.
+
+#include "test.hpp"
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <algorithm>
+
+char * StatusMessages[] = {
+	(char * )"Success",
+	(char * )"Failed",
+	(char * )"Unresolved",
+	(char * )"Timeout",
+	(char * )"Signaled",
+	(char * )"Unknown"
+	};
+	
+ProcessResult * Test::Execute(vector<string> args)
+{
+	ProcessResult * p_res = Process::Execute(args);
+	
+	TestResult * t_res = new TestResult(*p_res, _operation, _args);
+	
+	delete p_res;
+	
+	return t_res;
+}
+
+void TestCollection::Merge(TestCollection & tc)
+{
+	//for ( vector<Test *>::iterator i = tc._tests.begin(); i != tc._tests.end(); ++i)		
+	for ( unsigned int i = 0; i < tc._tests.size(); )
+	{
+		_tests.push_back(tc._tests[i]);
+		// Erase the original pointer so that the destructors do not overlap
+		tc._tests.erase(tc._tests.begin() + i);
+	}
+}
+
+TestResultCollection TestCollection::Run()
+{
+	TestResultCollection Results;
+		
+	for ( unsigned int index = 0 ; index < _tests.size(); ++index)
+	{
+		Results.AddResult( (TestResult *)_tests[index]->Execute() );
+	}	
+	
+	return Results;
+}
+
+string TestResult::StatusToString()
+{ 
+	if ( _status >= Success && _status <= Unknown )
+		return (string)StatusMessages[_status];
+	else
+		return (string)StatusMessages[Unknown];
+}
+
+string TestResult::ToXML()
+{
+	return OperationToString() + " : " + StatusToString() + " : " + _output + " : Arguments: " + _arguments;
+}
