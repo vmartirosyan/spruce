f30c39f3538c71b6119bc6c3e1e74f34f81dc8c4
raser <email.address@domain.net>
2011-08-08 15:49:26 +0500

    StatTest added

---
+++ src/syscall/stat/StatTest.hpp
@@ -0,0 +1,29 @@
+
+#ifndef TEST_STATTEST_H
+#define TEST_STATTEST_H
+
+#include "SyscallTest.hpp"
+// Operations
+enum StatSyscalls
+{
+    STAT_NORM_EXEC,
+ 
+};
+
+class StatTest : public SyscallTest
+{
+	public:
+		StatTest(Mode mode, int operation, string arguments = "") :
+            SyscallTest(mode, operation, arguments, "stat") {}
+		virtual ~StatTest() {}
+        Status NormExec();
+        /*
+        Status SetGetFlags(int i);
+        Status InvalidFD();
+        Status InvalidArgp();*/
+	protected:
+		virtual int Main(vector<string> args);
+};
+
+#endif /* TEST_STATTEST_H */
+ 
