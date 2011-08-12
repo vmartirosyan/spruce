f7c50b58ee16d20e258378cc8b67d9d37ab7cc0d
vmartirosyan <vmartirosyan@localhost>
2011-08-12 15:00:18 +0500

    Merge

--- src/syscall/ioctl/IoctlTest.hpp
+++ src/syscall/ioctl/IoctlTest.hpp
@@ -1,6 +1,9 @@
 //      DupFileDescriptor.hpp
 //
-//      Copyright 2011 Shahzadyan Khachik <qwerity@gmail.com>
+//		Copyright (C) 2011, Institute for System Programming
+//                          of the Russian Academy of Sciences (ISPRAS)
+//      Author:
+//				Shahzadyan Khachik <qwerity@gmail.com>
 //
 //      This program is free software; you can redistribute it and/or modify
 //      it under the terms of the GNU General Public License as published by
