a4b98b84ad956f7416fd99186739cfb71ae41081
ani.tumanyan <ani.tumanyan@localhost>
2011-08-16 12:52:13 +0500

    test for unlinkat

---
+++ src/syscall/ioctl/IoctlTest.hpp
@@ -0,0 +1,63 @@
+
+#ifndef TEST_IOCTL_H
+#define TEST_IOCTL_H
+
+#include "SyscallTest.hpp"
+// Operations
+enum IoctlSyscalls
+{
+    IOCTL_FS_SECRM_FL, /* Secure deletion */
+    IOCTL_FS_UNRM_FL,	/* Undelete */
+	IOCTL_FS_COMPR_FL,	/* Compress file */
+    IOCTL_FS_SYNC_FL,	 /* Synchronous updates */
+    IOCTL_FS_IMMUTABLE_FL,	 /* Immutable file */
+    IOCTL_FS_APPEND_FL,	 /* writes to file may only append */
+    IOCTL_FS_NODUMP_FL,	 /* do not dump file */
+    IOCTL_FS_NOATIME_FL,	 /* do not update atime */
+
+    IOCTL_FS_DIRTY_FL,		
+    IOCTL_FS_COMPRBLK_FL,	 /* One or more compressed clusters */
+    IOCTL_FS_NOCOMP_FL,	 /* Don't compress */
+    IOCTL_FS_ECOMPR_FL,	 /* Compression error */
+
+    IOCTL_FS_BTREE_FL,		 /* btree format dir */
+    IOCTL_FS_INDEX_FL,		 /* hash-indexed directory */
+    IOCTL_FS_IMAGIC_FL,	 /* AFS directory */
+    IOCTL_FS_JOURNAL_DATA_FL,	 /* Reserved for ext3 */
+    IOCTL_FS_NOTAIL_FL,		 /* file tail should not be merged */
+    IOCTL_FS_DIRSYNC_FL,		 /* dirsync behaviour (directories only) */
+    IOCTL_FS_TOPDIR_FL,		 /* Top of directory hierarchies*/
+    IOCTL_FS_EXTENT_FL,		 /* Extents */
+    IOCTL_FS_DIRECTIO_FL,		 /* Use direct i/o */
+    IOCTL_FS_RESERVED_FL,		 /* reserved for ext2 lib */
+    
+    IOCTL_FIOCLEX,
+    IOCTL_FIONCLEX,
+    IOCTL_FIONBIO,
+    IOCTL_FIOASYNC,
+    IOCTL_FIOQSIZE,
+    IOCTL_FIFREEZE,
+    IOCTL_FITHAW,
+    IOCTL_FS_IOC_FIEMAP,
+    IOCTL_FIGETBSZ,
+    
+    IOCTL_INVALID_FD,
+    IOCTL_INVALID_ARGP,
+
+};
+
+class IoctlTest : public SyscallTest
+{
+	public:
+		IoctlTest(Mode mode, int operation, string arguments = "") :
+			SyscallTest(mode, operation, arguments, "ioctl"){}
+		virtual ~IoctlTest() {}
+        
+        Status SetGetFlags(int i);
+        Status InvalidFD();
+        Status InvalidArgp();
+	protected:
+		virtual int Main(vector<string> args);
+};
+
+#endif /* TEST_IOCTL_H */
