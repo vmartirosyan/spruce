fcd7f863463f22520c8387f5f7d8489d2a8cfa5d
Vahram Martirosyan <vmartirosyan@gmail.com>
2012-02-09 14:53:05 +0400

    Fixed readv failure.

--- src/syscall/readv_writev/ReadvWritev.cpp
+++ src/syscall/readv_writev/ReadvWritev.cpp
@@ -381,15 +381,17 @@ Status ReadvWritev::ReadvEinvalErrorFunc3()
 // attempt to readv, when sum of iov_len values overflows an ssize_t value
 Status ReadvWritev::ReadvEinvalErrorFunc4()
 {	
-    string buf1;
-	string buf2;
+    char buf1[] = "asdf";
+    char buf2[] = "qwerty";
 		
 	struct iovec iovAr[BUF_COUNT];
 	
-	iovAr[0].iov_base = const_cast<char *>(buf1.c_str());
-	iovAr[0].iov_len = SSIZE_MAX/2;
-	iovAr[1].iov_base = const_cast<char *>(buf2.c_str());
-	iovAr[1].iov_len = SSIZE_MAX/2+2;
+	iovAr[0].iov_base = buf1;
+	iovAr[0].iov_len = 0xFFFFFFFF;
+	iovAr[1].iov_base = buf2;
+	iovAr[1].iov_len = 1;
+	
+	cerr << "SSIZE_MAX = " << SSIZE_MAX << endl;
 	
 	try
 	{
@@ -412,7 +414,7 @@ Status ReadvWritev::ReadvEinvalErrorFunc4()
 				
 		if (errno != EINVAL)
 		{
-			cerr << "Expecting to get EINVAL error, but wrong error set in errno:"<<strerror(errno) <<"errno="<<errno<<"  "<<EBADF<<endl;
+			cerr << "Expecting to get EINVAL error, but wrong error set in errno: "<< strerror(errno) <<" errno=" << errno <<"  "<< EBADF << endl;
 			return Fail;
 		}
 	} 
@@ -639,7 +641,7 @@ Status ReadvWritev::WritevEinvalErrorFunc3()
 	struct iovec iovAr[BUF_COUNT];
 	
 	iovAr[0].iov_base = const_cast<char *>(buf1.c_str());
-	iovAr[0].iov_len = SSIZE_MAX;
+	iovAr[0].iov_len = 0xFFFFFFFF;
 	iovAr[1].iov_base = const_cast<char *>(buf2.c_str());
 	iovAr[1].iov_len = 1;
 	
