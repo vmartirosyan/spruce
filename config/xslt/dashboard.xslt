3e6f1dd481f6528b9edb5b1b336e5ee1ebbce89d
Vahram Martirosyan <vmartirosyan@gmail.com>
2012-12-21 14:22:38 -0800

    syscall/acl: Added check for JFS on default ACL test.

---
+++ config/xslt/dashboard.xslt
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<xsl:output
+    method="html"    
+    doctype-public="-//W3C//DTD XHTML 1.0 Transitional//EN"
+    indent="yes"
+    encoding="utf-8" />
+    
+<xsl:key name="DistinctMountOptions" match="/SpruceDashboard/MountOptions/Option" use="." />
+
+<xsl:template match="/SpruceDashboard">
+<html>
+	<head>
+		<style>
+			.Success
+			{
+				color:Green;
+			}
+			.Shallow
+			{
+				color:Lime;
+			}
+			.Unresolved
+			{
+				color:Orange;
+			}
+			.Failed
+			{
+				color:Red;
+			}
+			.Unsupported
+			{
+				color:Gray;
+			}
+			.Timeout
+			{
+				color:Blue;
+			}
+		</style>
+		<title>Spruce dashboard for filesystem <xsl:value-of select="@FS" /></title>
+	</head>
+	<body>
+		<h1 style="text-align:center">Spruce dashboard for filesystem <b><xsl:value-of select="@FS" /></b></h1>
+		<h2 style="text-align:center">Start: <xsl:value-of select="Start" /></h2>
+		<h2 style="text-align:center">Duration: <xsl:value-of select="Duration" />s</h2>
+		<h2 style="text-align:center">Revision: <xsl:value-of select="Rev" /></h2>
+		<table border="1" cellspacing="0" align="center">
+			<tr>
+				<th>MountOptions</th>
+				<xsl:for-each select="Modules/Module">
+					<th><xsl:value-of select="."/></th>
+				</xsl:for-each>
+			</tr>
+			<xsl:for-each select="//MountOptions/Option[generate-id()=generate-id(key('DistinctMountOptions',.)[1])]">
+				<xsl:variable name="MountOptions" select="." />
+				<tr>
+					<td>
+						<xsl:value-of select="."/>
+					</td>
+					<xsl:for-each select="//Modules/Module">
+						<td>
+							<xsl:variable name="LogFile"><xsl:value-of select="$LogFolder"/>/<xsl:value-of select="/SpruceDashboard/@FS"/>_<xsl:value-of select="."/>_<xsl:value-of select="$MountOptions"/>_log.xml</xsl:variable>
+							<xsl:variable name="LogFileHtml"><xsl:value-of select="$LogFolder"/>/<xsl:value-of select="/SpruceDashboard/@FS"/>_<xsl:value-of select="."/>_<xsl:value-of select="$MountOptions"/>_log.html</xsl:variable>
+							<xsl:choose>
+								<xsl:when test="document($LogFile)">
+									<a>
+										<xsl:attribute name="href"><xsl:value-of select="$LogFileHtml"/></xsl:attribute>
+										<span class="Success">
+											<xsl:value-of select="count(document($LogFile)//Item[Status='Success' or Status='Shallow' or Status='FSimFail'])" /> 
+										</span> / 
+										<span class="Failed">
+											<xsl:value-of select="count(document($LogFile)//Item[Status='Fatal' or Status='Failed' or Status='Signaled' or Status='Timeout' or Status='FSimSuccess'])" /> 
+										</span> / 
+										<span class="Unresolved">
+											<xsl:value-of select="count(document($LogFile)//Item[Status='Unresolved' or Status='Unsupported' or Status='Skipped'])" />
+										</span>
+									</a>
+								</xsl:when>
+								<xsl:otherwise>
+									-
+								</xsl:otherwise>
+							</xsl:choose>
+						</td>
+					</xsl:for-each>
+				</tr>
+			</xsl:for-each>
+		</table>		
+	</body>
+</html>
+</xsl:template>
+
+</xsl:stylesheet>
---
+++ include/LeakChecker.hpp
@@ -0,0 +1,190 @@
+//      leak_checker.hpp
+//      
+//      Copyright (C) 2011, Institute for System Programming
+//                          of the Russian Academy of Sciences (ISPRAS)
+//      Author:
+//			Vahram Martirosyan <vmartirosyan@gmail.com>
+//      	Ruzanna Karakozova <r.karakozova@gmail.com>
+//
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//      
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//      
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//      MA 02110-1301, USA.
+
+#include <Exception.hpp>
+#include <UnixCommand.hpp>
+#include <sys/utsname.h>
+#include <algorithm>
+
+using namespace std;
+
+// A class which processes KEDR leak check output
+class LeakChecker
+{
+public:
+	LeakChecker(string log_file = ""):
+		DebugFSPath(""),
+		LogFilePath(log_file)
+	{
+		MountDebugFS();
+	}
+	void ProcessLeakCheckerOutput()
+	{
+		int status = Success;
+		try
+		{
+			int fd = -1;
+			if((fd = open(LogFilePath.c_str(), O_RDWR)) == -1)
+			{
+				throw Exception(string("open log file failed"));
+			}
+			
+			struct stat sb;
+			if(fstat(fd, &sb) == -1)
+			{
+				throw Exception(string("stat failed"));
+			}
+			
+			if(ftruncate(fd, sb.st_size - 9) == -1)
+			{
+				throw Exception(string("ftruncate failed"));
+			}
+			
+			close(fd);
+			
+			ofstream of(LogFilePath.c_str(), ios_base::app);
+			// First process the possible leaks
+			ifstream in_file((DebugFSPath + "/kedr_leak_check/possible_leaks").c_str());
+			string line;
+			getline(in_file, line);
+			while (in_file.good() && !in_file.eof())
+			{					
+				if ( line.find("Address:") == string::npos )
+				{
+					status = Fail;
+					getline(in_file, line);
+					continue;
+				}
+
+				cerr << "Leak checker. Adding item." << endl;
+				of << "\t<Item Name=\"PossibleLeak\" Id=\"" << rand() << "\">\n\t\t<Status>Failed</Status><Output>";
+
+				string Data;
+				do
+				{
+					//process line to valid xml
+					//cerr << "Line: " << line << endl;
+					size_t pos = 0;
+					while ( ( pos = line.find("<", pos ) ) != string::npos )
+					{
+						line = line.replace(pos, 1, "&lt;");
+					}
+					
+					Data += line + "\n";
+					
+					getline(in_file, line);
+				}
+				while(line.find("Address:") == string::npos && !in_file.eof());
+				
+				// Write data
+				of << Data << "</Output>\n\t</Item>";				
+			}
+			in_file.close();	
+			
+			// Then the unallocated frees will come
+			in_file.open((DebugFSPath + "/kedr_leak_check/unallocated_frees").c_str());
+			getline(in_file, line);
+			while (in_file.good() && !in_file.eof())
+			{					
+				if ( line.find("Address:") == string::npos )
+				{
+					status = Fail;
+					getline(in_file, line);
+					continue;
+				}
+
+				cerr << "Leak checker. Adding item." << endl;
+				of << "\t<Item Name=\"UnallocatedFree\" Id=\"" << rand() << "\">\n\t\t<Status>Failed</Status><Output>";
+
+				string Data;
+				do
+				{
+					//process line to valid xml
+					//cerr << "Line: " << line << endl;
+					size_t pos = 0;
+					while ( ( pos = line.find("<", pos ) ) != string::npos )
+					{
+						line = line.replace(pos, 1, "&lt;");
+					}
+					
+					Data += line + "\n";
+					
+					getline(in_file, line);
+				}
+				while(line.find("Address:") == string::npos && !in_file.eof());
+				
+				// Write data
+				of << Data << "</Output>\n\t</Item>";
+			}
+			in_file.close();
+			if (status == Success)
+			{
+				of << "\t<Item Name=\"LeakChecker\" Id=\"" << rand() << "\">\n\t\t<Status>Success</Status><Output>No leaks or unallocated frees were found</Output></Item>";
+			}
+			of << "</Module>";
+			of.close();
+			
+		}
+		catch(exception& err)
+		{
+			cerr << "Exception is thrown." << err.what() << endl;
+			return;
+		}
+	}
+protected:
+	string DebugFSPath;
+	string LogFilePath;
+	
+	bool MountDebugFS()
+	{
+		// Is DebugFS already mounted?
+		UnixCommand mount("mount");
+		vector<string> args;
+		args.push_back("-t");
+		args.push_back("debugfs");
+				
+		ProcessResult * res = mount.Execute(args);
+		
+		if ( res == NULL || res->GetStatus() != Success )
+			throw Exception("Error executing mount. " + (res ? res->GetOutput() : ""));
+			
+		string Output = res->GetOutput();
+		if ( Output != "" )
+		{
+			// Read the path
+			int SpacePos = Output.find(' ', 9);
+			DebugFSPath = Output.substr(8, SpacePos - 8);
+			cout << "DebugFS path : " << DebugFSPath << endl;
+			return true;
+		}
+			
+		// Try to mount...
+		args.push_back(DebugFSPath);
+		res = mount.Execute(args);
+		
+		if ( res == NULL || res->GetStatus() != Success )
+			throw Exception("Error executing mount. " + (res ? res->GetOutput() : ""));
+			
+		return true;
+	}
+};
