48fed0a0323766a8a7b06d3fa8845d069c0dcd89
Andrey Tsyvarev <tsyvarev@ispras.ru>
2013-02-15 13:35:52 +0400

    report.pl: When create directory for report, also create subdirectories if needed ('mkdir -p' semantic).
    Also add copyright notes.

--- report_generator/report.pl
+++ report_generator/report.pl
@@ -1,5 +1,28 @@
 #!/usr/bin/perl -w 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#      Copyright (C) 2013, Institute for System Programming
+#                          of the Russian Academy of Sciences (ISPRAS)
+#      Authors:
+#			Andrey Tsyvarev <tsyvarev@ispras.ru>
+#			Alexey Khoroshilov <khoroshilov@ispras.ru>
+#      
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation; either version 2 of the License, or
+#      (at your option) any later version.
+#      
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#      
+#      You should have received a copy of the GNU General Public License
+#      along with this program; if not, write to the Free Software
+#      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#      MA 02110-1301, USA.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 use warnings;
 
@@ -7,6 +30,8 @@ use HTML::Template; # For template-generation output files.
 
 use XML::Parser; # For parse Spruce logs.
 use Getopt::Long; # For parse options
+
+use File::Path qw(make_path); # Create directories like 'mkdir -p'.
 
 #---------------------------------------------------------------------
 # Terminate program as with fatal error.
@@ -1825,7 +1850,7 @@ sub compile_report
 {
     my $result_dir = shift;
     
-    mkdir("$result_dir");
+    make_path("$result_dir");
     
     if(@other_problems)
     {
