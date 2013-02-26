67768469200532ee2763273fdc3154bcbe3999c3
Giorgi Gulabyan <gulabyang@gmail.com>
2013-02-26 16:18:17 +0400

    Added COMPAT definition

---
+++ cmake/arch.cmake
@@ -0,0 +1,50 @@
+function (arch module)
+	IF ( ${OS_32_BITS} OR NOT ${HAVE_MULTILIB})	
+	ADD_EXECUTABLE(${module} ${CMAKE_CURRENT_BINARY_DIR}/module.cpp )
+
+	TARGET_LINK_LIBRARIES(${module} utils)
+	if (${module} STREQUAL "syscall" OR ${module} STREQUAL "fault_sim")
+		TARGET_LINK_LIBRARIES(${module} acl)
+	endif()
+	
+	
+	install (	FILES "${CMAKE_BINARY_DIR}/bin/${module}"
+				DESTINATION bin
+				PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ WORLD_EXECUTE WORLD_READ)
+	ELSE ( ${OS_32_BITS} OR NOT ${HAVE_MULTILIB})
+		ADD_EXECUTABLE(${module}_64 ${CMAKE_CURRENT_BINARY_DIR}/module.cpp)
+		TARGET_LINK_LIBRARIES(${module}_64 utils)
+		if (${module} STREQUAL "syscall")
+			TARGET_LINK_LIBRARIES(${module}_64 acl)
+		endif()
+
+		install (	FILES "${CMAKE_BINARY_DIR}/bin/${module}_64"
+				DESTINATION bin
+				PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ WORLD_EXECUTE WORLD_READ)
+
+	ENDIF ( ${OS_32_BITS} OR NOT ${HAVE_MULTILIB})	
+
+	IF (NOT ${OS_32_BITS} AND ${HAVE_MULTILIB})	
+		ADD_CUSTOM_COMMAND(
+	   OUTPUT  module_32.cpp
+	   COMMAND cp module.cpp module_32.cpp
+	   DEPENDS module.cpp
+	   )
+		set_source_files_properties(module_32.cpp PROPERTIES COMPILE_FLAGS "-m32")
+		
+		ADD_DEFINITIONS(-DCOMPAT)
+		ADD_EXECUTABLE(${module}_32 module_32.cpp)
+		TARGET_LINK_LIBRARIES(${module}_32 utils_32)
+		if (${module} STREQUAL "syscall")
+			TARGET_LINK_LIBRARIES(${module}_32 acl)
+		endif()	
+		
+		set_target_properties(${module}_32 PROPERTIES LINK_FLAGS "-m32") 
+		
+		install (	FILES "${CMAKE_BINARY_DIR}/bin/${module}_32"
+				DESTINATION bin
+				PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ WORLD_EXECUTE WORLD_READ)
+		
+	ENDIF (NOT ${OS_32_BITS} AND ${HAVE_MULTILIB})
+
+endfunction()
---
+++ include/Test.hpp
@@ -0,0 +1,228 @@
+//      Test.hpp
+//      
+//      Copyright (C) 2011, Institute for System Programming
+//                          of the Russian Academy of Sciences (ISPRAS)
+//      Author:
+//			Vahram Martirosyan <vmartirosyan@gmail.com>
+//      
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//      
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//      
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//      MA 02110-1301, USA.
+
+#ifndef TEST_BASE_H
+#define TEST_BASE_H
+
+#include "Common.hpp"
+#include "Process.hpp"
+#include <stdlib.h>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+class TestResult : public ProcessResult
+{
+public:
+	/*TestResult():
+		ProcessResult(Unknown, "No output"),
+		_operation(0),
+		_arguments("No arguments provided") {}*/
+	TestResult(ProcessResult pr, int op, string args, string desc, string spec = "", string stroper = ""):
+		ProcessResult(pr),
+		_spec(spec),
+		_operation(op),
+		_description(desc),
+		_stroperation(stroper),
+		_arguments(args) {}
+	
+	TestResult(ProcessResult pr, string args, string spec, string stroper, string desc):
+		ProcessResult(pr),
+		_spec(spec),
+		_operation(0),
+		_description(desc),
+		_stroperation(stroper),
+		_arguments(args) {}
+		
+	TestResult(Status s, string output, int op, string args, string desc):
+		ProcessResult(s, output),
+		_spec(""),
+		_operation(op),
+		_description(desc),
+		_stroperation(OperationToString()),
+		_arguments(args) {}
+	TestResult(ProcessResult pr, string op, string args, string desc):
+		ProcessResult(pr),
+		_spec(""),
+		_operation(0),
+		_description(desc),
+		_stroperation(op),		
+		_arguments(args)
+		 {}
+	TestResult(Status s, string output, string op, string args, string desc):
+		ProcessResult(s, output),
+		_spec(""),
+		_operation(0),
+		_description(desc),
+		_stroperation(op),
+		_arguments(args) {}
+	TestResult(TestResult const & tr) : 
+		ProcessResult(tr),
+		_spec(tr._spec),
+		_operation(tr._operation),
+		_description(tr._description),
+		_stroperation(tr._stroperation),
+		_arguments(tr._arguments) {}
+	 	 	 
+	//virtual string ToXML();
+	virtual string OperationToString()
+	{
+		return "Unknown";
+		//return Operation::ToString((Operations)_operation);
+	}
+	string GetStrOperation() const
+	{
+		return _stroperation;
+	}
+	string GetArguments() const
+	{
+		return _arguments;
+	}
+	string GetDescription() const
+	{
+		return _description;
+	}
+	virtual string ToXML();
+	string GetSpec() const
+	{
+		return _spec;
+	}
+protected:
+	string _spec;
+	int _operation;
+	string _description;
+	string _stroperation;
+	string _arguments;
+};
+
+class TestResultCollection 
+{
+public:
+	TestResultCollection():
+		_results()
+		{}
+	void AddResult(Status s, string output, int op, string args, string desc)
+	{
+		TestResult * tmp = new TestResult(s,output,op,args, desc);
+		_results.push_back(tmp);
+	}
+	void AddResult(TestResult * result)
+	{
+		_results.push_back(result);
+	}
+	void Merge(TestResultCollection results)
+	{
+		for ( unsigned int i = 0; i < results._results.size(); )
+		{
+			_results.push_back(results._results[i]);
+			// Erase the original pointer so that the destructors do not overlap
+			results._results.erase(results._results.begin() + i);
+		}
+	}
+	string ToXML()
+	{
+		string result = "";
+		for ( vector<TestResult *>::iterator i = _results.begin(); i != _results.end(); ++i )
+		{
+			//cerr << (*i)->ToXML() << endl;
+			result += (*i)->ToXML();			
+		}
+		
+		return result;
+	}
+	// Returns the highest status in the collection.
+	Status GetStatus()
+	{
+		Status max_stat = Success;
+		for ( vector<TestResult *>::iterator i = _results.begin(); i != _results.end(); ++i )
+			if ( (*i)->GetStatus() > max_stat && (*i)->GetStatus() >= Unresolved )
+				max_stat = (*i)->GetStatus();
+		return max_stat;
+	}
+	string GetOutput() const
+	{
+		return "asdf";
+	}
+	~TestResultCollection()
+	{
+		for ( vector<TestResult *>::iterator i = _results.begin(); i != _results.end(); ++i )
+			delete (*i);
+	}
+	const vector<TestResult*>& GetResults() const
+	{
+		return _results;
+	}
+private:
+	vector<TestResult*> _results;
+};
+
+/*
+class Test : public Process
+{
+public:	
+	Test(Mode m, int op, string a):
+		_mode(m),
+		_operation(op),
+		_stroperation("Unknown"),
+		_args(a) {}
+	Test(Mode m, string op, string a):
+		_mode(m),
+		_operation(0),
+		_stroperation(op),
+		_args(a) {}
+	Test(const Test & t):
+		_mode(t._mode),
+		_operation(t._operation),
+		_stroperation(t._stroperation),
+		_args(t._args) {}
+	
+	ProcessResult * Execute(vector<string> args = vector<string>());
+	virtual ~Test() {}
+protected:
+	virtual int Main(vector<string> args) = 0;
+	Mode _mode;
+	int _operation;
+	string _stroperation;
+	string _args;
+};
+
+class TestCollection
+{
+public:
+	TestResultCollection Run();
+	void AddTest(Test * t)
+	{
+		_tests.push_back(t);
+	}
+	void Merge( TestCollection &);
+	~TestCollection()
+	{
+		for ( vector<Test *>::iterator i = _tests.begin(); i != _tests.end(); ++i )
+		{
+			delete (*i);
+		}
+	}	
+private:
+	vector<Test *> _tests;
+};
+*/
+#endif /* TEST_BASE_H */
