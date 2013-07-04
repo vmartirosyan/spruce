16d5439d533135a63d88397f92763c74ff036f97
Andrey Tsyvarev <tsyvarev@ispras.ru>
2013-07-04 15:51:21 +0400

    Fix configuration of __dquot_alloc_space kernel function for payload.
    Fix names of probe files and include directive in these files.

---
+++ src/kedr_payloads/check_functions/d_alloc_root.c
@@ -0,0 +1,26 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#include <linux/fs.h>
+
+MODULE_LICENSE("GPL");
+
+static int __init
+my_init(void)
+{
+    // Call target function
+    struct inode* i = NULL;
+    struct dentry* d = d_alloc_root(i);
+    dput(d);
+
+    return 0;
+}
+
+static void __exit
+my_exit(void)
+{
+}
+
+module_init(my_init);
+module_exit(my_exit);
